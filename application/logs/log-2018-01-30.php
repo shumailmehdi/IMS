0cb5cde186929c2b3b7c0a6c175e8ce490d4fdbb
Syed Shumail Mehdi <ssmehdi.95@gmail.com>
2018-01-30 15:25:55 +0500

    sadasdasd
    Files Added

---
+++ application/logs/log-2018-01-30.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-01-30 08:29:34 --> 404 Page Not Found: Public/images
+ERROR - 2018-01-30 08:30:10 --> 404 Page Not Found: Public/images
